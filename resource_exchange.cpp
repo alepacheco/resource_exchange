14fe7527efd6617cd7718a91a7e0989f00b8d9c9
Your Name <you@example.com>
2018-06-26 13:06:29 +0200

    Fixed delegate function

--- resource_exchange.cpp
+++ resource_exchange.cpp
@@ -87,7 +87,7 @@ void resource_exchange::delegatebw(account_name receiver,
                                    asset stake_cpu_quantity) {
   action(permission_level(_contract, N(active)), N(eosio), N(delegatebw),
          std::make_tuple(_contract, receiver, stake_net_quantity,
-                         stake_cpu_quantity))
+                         stake_cpu_quantity, false))
       .send();
 }
 // FIXME
